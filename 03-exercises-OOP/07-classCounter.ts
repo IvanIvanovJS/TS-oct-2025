260c4706582fab566af2d20b20f95e667c02975d
IvanIvanovJS <sibsmartco@gmail.com>
2025-12-01 15:41:33 +0200

    solved (07-classCounter) exercise

---
+++ 03-exercises-OOP/07-classCounter.ts
@@ -0,0 +1,15 @@
+class Counter {
+	private static count: number = 0;
+
+	static increment() {
+		this.count++;
+	}
+
+	static getCount(): number {
+		return this.count;
+	}
+}
+
+Counter.increment();
+Counter.increment();
+console.log(Counter.getCount());
